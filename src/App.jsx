0ae22be17374f79c78d81135e84448589a45b5d4
Waltsuuu <waltteriheino@gmail.com>
2024-08-23 10:04:36 +0300

    Added proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,61 @@
 import { useState } from 'react'
+import { PropTypes } from 'prop-types'
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+History.propTypes = {
+  allClicks: PropTypes.array,
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+Button.propTypes = {
+  handleClick: PropTypes.func,
+  text: PropTypes.string
+}
 
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
    
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
     const updatedLeft = left + 1
     setLeft(updatedLeft)
-    setTotal(updatedLeft + right) 
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     const updatedRight = right + 1
     setRight(updatedRight)
-    setTotal(updatedRight + left) 
   }
 
   return (
     <div>
       <div>
-        {left}
-        <button onClick={handleLeftClick}>
-        Left
-        </button>
-        <button onClick={handleRightClick}>
-        Right
-        </button>
+      {left}
+        <Button handleClick={handleLeftClick} text='left' />
+        <Button handleClick={handleRightClick} text='right' />
         {right}
-        <p>{allClicks.join('')}</p>
-        <p>Total {total}</p>
+        <History allClicks={allClicks} />
       </div>
     </div>
   )
